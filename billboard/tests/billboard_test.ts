6bc0bfdd1269812c628b68ced96521d66e87e4d5
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-06-23 20:41:04 +0300

    feat(billboard): add implementation

--- billboard/tests/billboard_test.ts
+++ billboard/tests/billboard_test.ts
@@ -1,26 +1,37 @@
-
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.12.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "A quick demo on how to assert expectations",
     async fn(chain: Chain, accounts: Map<string, Account>) {
+        let wallet_1 = accounts.get('wallet_1')!;
+
+        let assetMaps = chain.getAssetsMaps();
+        const balance = assetMaps.assets['STX'][wallet_1.address];
+
         let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
+           Tx.contractCall('billboard', 'set-message', [types.utf8("testing")], wallet_1.address),
+           Tx.contractCall('billboard', 'get-message', [], wallet_1.address),
+           Tx.contractCall('billboard', 'set-message', [types.utf8("testing...")], wallet_1.address),
+           Tx.contractCall('billboard', 'get-message', [], wallet_1.address),
         ]);
-        assertEquals(block.receipts.length, 0);
+
+        assertEquals(block.receipts.length, 4);
         assertEquals(block.height, 2);
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+        block.receipts[1].result
+            .expectUtf8('testing');
+
+        block.receipts[3].result
+            .expectUtf8('testing...');
+        
+        let [event] = block.receipts[0].events;
+        let {sender, recipient, amount} = event.stx_transfer_event;
+        sender.expectPrincipal("ST1J4G6RR643BCG8G8SR6M2D9Z9KXT2NJDRK3FBTK");
+        recipient.expectPrincipal("ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.billboard");
+        amount.expectInt(100);
+
+        assetMaps = chain.getAssetsMaps();
+        assertEquals(assetMaps.assets['STX'][wallet_1.address], balance - 210);
     },
 });
