18ce0d3f0e9b23572094ae290eae18dc3a8382ba
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-06-28 09:13:06 +0300

    chore(docker): remove complex scenario

--- provisioning/bitcoin/docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/local/bin/bitcoind -conf=/etc/bitcoin/bitcoin.conf -pid=/run/bitcoind.pid -datadir=/root/.bitcoin
--- provisioning/stacks-blockchain-leader/docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/bin/wait && /bin/stacks-node start --config /src/stacks-node/Config.toml
