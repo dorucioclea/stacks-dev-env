7c8ab803f4f13b081c028b1243fd4e539e6fc101
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-06-30 20:39:53 +0300

    feat(transaction): add helper functions

--- shared/transaction.ts
+++ shared/transaction.ts
@@ -75,3 +75,21 @@ export type Response<Ok, Err> = ResponseOk<Ok> | ResponseErr<Err>;
 export interface Transaction<Ok, Err> {
   submit: Submitter<Ok, Err>;
 }
+
+export async function tx<A, B>(tx: Transaction<A, B>, sender: string) {
+  const receipt = await tx.submit({ sender });
+  const result = await receipt.getResult();
+  return result;
+}
+
+export async function txOk<A, B>(_tx: Transaction<A, B>, sender: string) {
+  const result = await tx(_tx, sender);
+  if (!result.isOk) throw new Error(`Expected transaction ok, got error: ${result.value}`);
+  return result;
+}
+
+export async function txErr<A, B>(_tx: Transaction<A, B>, sender: string) {
+  const result = await tx(_tx, sender);
+  if (result.isOk) throw new Error(`Expected transaction error, got ok: ${result.value}`);
+  return result;
+}
