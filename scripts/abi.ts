0028a6ea8158a9efd106e87ca3389a952b4c2077
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-06-30 18:12:49 +0300

    chore(abi): add abi test

--- scripts/abi.ts
+++ scripts/abi.ts
@@ -1,33 +1,10 @@
-import { convertContractFile } from "../dos2unix";
-import { generateFilesForContract } from "../shared/abi/generate-for-contract";
+import { generateAbis } from "./generate-abi";
 
 describe("Generate abi", async () => {
     try {
-
-        convertContractFile('counter');
-
-        await generateFilesForContract({
-            contractFile: "contracts//counter.clar",
-            outputFolder:"out",
-            contractAddress: 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH'
-          });
+        generateAbis();
        } catch(error) {
             console.log('found error  ', error);
        }   
 });
 
-
-// async function generate() {
-//     try {
-//         await generateFilesForContract({
-//             contractFile: "counter.clar",
-//             outputFolder:"/out",
-//             contractAddress: 'ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH',
-//             dirName: './contracts'
-//           });
-//        } catch(error) {
-//             console.log('found error  ', error);
-//        }
-// }
-
-// generate();
