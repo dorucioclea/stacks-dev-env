5d9d836c4b34c4a04ada0a74a322ee03e5774f66
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-01 09:14:45 +0300

    feat(contracts): specify all contracts

--- scripts/contracts.ts
+++ scripts/contracts.ts
@@ -1,2 +1,11 @@
 
-export type CONTRACTS = "counter" | "counter-coin" | "ft-trait";
+export type CONTRACTS = "simple-counter" | "counter" | "counter-coin" | "sip-10-ft-standard";
+
+export const GENERATION_FOLDER = "src//";
+
+export const contracts: CONTRACTS[] = [
+    "simple-counter",
+    "sip-10-ft-standard",
+    "counter-coin",
+    'counter',
+];
