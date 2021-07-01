189e06309214b4db8b9d7affa6e43f310d3aad31
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-01 09:31:05 +0300

    feat(abi-test): wip test with project file

--- scripts/abi.ts
+++ scripts/abi.ts
@@ -1,8 +1,13 @@
+import { contracts } from "./contracts";
+import { convertDos2Unix } from "./dos2unix";
 import { generateAbis } from "./generate-abi";
+import { generateProjectIndexFile } from "./generate-project";
 
 describe("Generate abi", async () => {
     try {
+        // await convertDos2Unix();
         generateAbis();
+        generateProjectIndexFile(contracts);
        } catch(error) {
             console.log('found error  ', error);
        }   
