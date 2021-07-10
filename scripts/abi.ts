916d98db2c1dbc8e127250e41909000367e88dda
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-10 11:39:59 +0300

    style(abi): remove dos2unix function import

--- scripts/abi.ts
+++ scripts/abi.ts
@@ -1,5 +1,4 @@
 import { contracts } from "./contracts";
-import { convertDos2Unix } from "./dos2unix";
 import { generateAbis } from "./generate-abi";
 import { generateProjectIndexFile } from "./generate-project";
 
