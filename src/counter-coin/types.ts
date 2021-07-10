67a13786f82166938298104c67007de6a442d609
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-10 11:37:21 +0300

    feat(types): include the metadata prop

--- src/counter-coin/types.ts
+++ src/counter-coin/types.ts
@@ -1,15 +1,16 @@
 import { Transaction } from '../../shared/transaction';
 import { ClarityTypes } from '../../shared/clarity/types';
+import { IMetadata } from '../../shared/providers/types';
 
 // prettier-ignore
 
 export interface CounterCoinContract {
-  getTokenUri: () => Transaction<string | null, null>;
-  mint: (recipient: string, amount: number) => Transaction<boolean, number>;
-  transfer: (amount: number, sender: string, recipient: string, memo: Buffer | null) => Transaction<boolean, number>;
-  getBalance: (owner: string) => Promise<ClarityTypes.Response<number, null>>;
-  getDecimals: () => Promise<ClarityTypes.Response<number, null>>;
-  getName: () => Promise<ClarityTypes.Response<string, null>>;
-  getSymbol: () => Promise<ClarityTypes.Response<string, null>>;
-  getTotalSupply: () => Promise<ClarityTypes.Response<number, null>>;
+  getTokenUri: (metadata?: IMetadata) => Transaction<string | null, null>;
+  mint: (recipient: string, amount: number, metadata?: IMetadata) => Transaction<boolean, number>;
+  transfer: (amount: number, sender: string, recipient: string, memo: Buffer | null, metadata?: IMetadata) => Transaction<boolean, number>;
+  getBalance: (owner: string, metadata?: IMetadata) => Promise<ClarityTypes.Response<number, null>>;
+  getDecimals: (metadata?: IMetadata) => Promise<ClarityTypes.Response<number, null>>;
+  getName: (metadata?: IMetadata) => Promise<ClarityTypes.Response<string, null>>;
+  getSymbol: (metadata?: IMetadata) => Promise<ClarityTypes.Response<string, null>>;
+  getTotalSupply: (metadata?: IMetadata) => Promise<ClarityTypes.Response<number, null>>;
 }
