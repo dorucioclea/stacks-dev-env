38e31ff8f6d9e5226d0e98408d3df39dae1feb68
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-11 15:03:15 +0300

    feat(deploy): ugh, reverting to previous addresses & configuration

--- scripts/deploy-utils.ts
+++ scripts/deploy-utils.ts
@@ -14,6 +14,7 @@ import {
   const fetch = require("node-fetch");
   
   import { ADDR1, testnetKeyMap } from "../configuration/testnet";
+import BN from "bn.js";
 
   const STACKS_CORE_API_URL = "http://localhost:3999";
   
@@ -85,7 +86,7 @@ import {
       senderKey: secretDeployKey,
       network,
       anchorMode: 3,
-      // fee: new BN(1000),
+      fee: new BN(1000),
       // nonce: new BN(10)
     });
   
