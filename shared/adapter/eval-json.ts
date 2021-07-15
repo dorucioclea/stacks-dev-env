04ea1a2fb16a1916775b049d03e30b3670749f57
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-07-15 15:49:17 +0300

    refactor(eval-json): split logic to reuse when calling variables

--- shared/adapter/eval-json.ts
+++ shared/adapter/eval-json.ts
@@ -12,19 +12,35 @@ export async function evalJson({
   provider: NativeClarityBinProvider;
   args?: string[];
 }) {
-  const evalCode = `(${functionName} ${args.join(" ")})`;
+  const evalCode = `(${functionName} ${args.join(' ')})`;
+  return evalWithCode({
+    evalCode,
+    provider,
+    contractAddress,
+  });
+}
+
+export const evalWithCode = async ({
+  evalCode,
+  provider,
+  contractAddress,
+}: {
+  evalCode: string;
+  provider: NativeClarityBinProvider;
+  contractAddress: string;
+}) => {
   const receipt = await provider.runCommand(
-    ["eval_at_chaintip", "--costs", contractAddress, provider.dbFilePath],
+    ['eval_at_chaintip', '--costs', contractAddress, provider.dbFilePath],
     {
       stdin: evalCode,
     }
   );
   const response: EvalResult = JSON.parse(receipt.stdout);
-  if (process.env.PRINT_CLARIGEN_STDERR && receipt.stderr) {
+  if (receipt.stderr) {
     console.log(receipt.stderr);
   }
   if (!response.success) {
-    throw new Error(JSON.stringify((response as EvalErr).error, null, 2));
+    throw new Error(JSON.stringify(response.error, null, 2));
   }
   return response;
-}
+};
